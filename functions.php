b86b9ab02acaa4d1448150a62165b33c6cf7f3b3
Nathan Rice <ncrice@gmail.com>
2013-04-02 00:33:25 -0300

    Change single line comment format

--- functions.php
+++ functions.php
@@ -1,15 +1,15 @@
 <?php
-//** Start the engine
+//* Start the engine
 require_once( get_template_directory() . '/lib/init.php' );
 
 define( 'CHILD_THEME_NAME', 'Genesis Starter Theme' );
 define( 'CHILD_THEME_URI', 'https://github.com/nathanrice/Genesis-Starter-Theme' );
 define( 'CHILD_THEME_VERSION', '0.2.0' );
 
-//** $content_width
+//* $content_width
 $content_width = 740;
 
-//** HTML5, please
+//* HTML5, please
 add_theme_support( 'genesis-html5' );
 
 #add_action( 'wp_head', 'nr_viewport_meta_tag' );
@@ -20,13 +20,13 @@ function nr_viewport_meta_tag() {
     echo '<meta name="viewport" content="width=device-width, initial-scale=1.0"/>' . "\n";
 }
 
-//** Custom Image Sizes
+//* Custom Image Sizes
 #add_image_size( 'image-size', 100, 100, true );
 
-//** Disable widgets
+//* Disable widgets
 #remove_action( 'widgets_init', 'genesis_load_widgets' );
 
-//** Remove layouts
+//* Remove layouts
 #genesis_unregister_layout( 'sidebar-content' );
 #genesis_unregister_layout( 'content-sidebar' );
 #genesis_unregister_layout( 'content-sidebar-sidebar' );
@@ -34,14 +34,14 @@ function nr_viewport_meta_tag() {
 #genesis_unregister_layout( 'sidebar-content-sidebar' );
 #genesis_unregister_layout( 'full-width-content' );
 
-//** Disable edit links
+//* Disable edit links
 add_filter( 'genesis_edit_post_link', '__return_false' );
 
-//** Move Primary Navigation
+//* Move Primary Navigation
 #remove_action( 'genesis_after_header', 'genesis_do_nav' );
 #add_action( 'genesis_before_header', 'genesis_do_nav' );
 
-//** Move Secondary Navigation
+//* Move Secondary Navigation
 #remove_action( 'genesis_after_header', 'genesis_do_subnav' );
 #add_action( 'genesis_footer', 'genesis_do_subnav' );
 
